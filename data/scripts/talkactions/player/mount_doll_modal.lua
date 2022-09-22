227bd69d00992038592ea6fb376f8a43db231e90
Leandro Pedroso <leandroppf@gmail.com>
2022-09-21 23:48:05 -0300

    Modificando mount dolls

--- data/scripts/talkactions/player/mount_doll_modal.lua
+++ data/scripts/talkactions/player/mount_doll_modal.lua
@@ -1,116 +1,170 @@
+config = {
+	expensiveDollID = 37743, -- Draptor doll
+	dollID = ,
+	cheapDollID = ,
+}
+
+local expensiveMounts = {
+
+	-- Config
+	-- Main Window Messages (The first window the player sees)
+	mainTitle = "Escolhe uma montaria seu rico!",
+	mainMsg = "Vai liberar o bichin na hora.",
+
+	-- Already Owned Window (The window that appears when the player already owns the addon)
+	ownedTitle = "EEEEEPA!",
+	ownedMsg = "Você ja tem essa.",
+
+	-- No Doll in Backpack (The window that appears when the player doesnt have the doll in their backpack)
+	dollTitle = "EEEEEPA!",
+	dollMsg = "Tem que ter um dos mount dolls espertinho",
+	-- End Config
+
+	-- Mounts Table
+	[1] = {name = "Antelope", ID = 163},
+	[2] = {name = "Azudocus", ID = 44},
+	[3] = {name = "Blazebringer", ID = 9},
+	[4] = {name = "Blue Rolling Barrel", ID = 156},
+	[5] = {name = "Red Rolling Barrel", ID = 157},
+	[6] = {name = "Green Rolling Barrel", ID = 158},
+	[7] = {name = "Cold Percht Sleigh Final", ID = 150},
+	[8] = {name = "Bright Percht Sleigh Final", ID = 151},
+	[9] = {name = "Dark Percht Sleigh Final", ID = 152},
+	[10] = {name = "Carpacosaurus", ID = 45},
+	[11] = {name = "Cerberus Champion", ID = 146},
+	[12] = {name = "Fleeting Knowledge", ID = 126},
+	[13] = {name = "Glooth Glider", ID = 71},
+	[14] = {name = "Ironblight", ID = 29},
+	[15] = {name = "Jousting Eagle", ID = 145},
+	[16] = {name = "Krakoloss", ID = 175},
+	[17] = {name = "Magma Crawler", ID = 30},
+	[18] = {name = "Phant", ID = 182},
+	[19] = {name = "Phantasmal Jade", ID = 167},
+	[20] = {name = "Rift Runner", ID = 87},
+	[21] = {name = "Noxious Ripptor", ID = 202},
+	[22] = {name = "Shellodon", ID = 183},
+	[23] = {name = "Shock Head", ID = 42},
+	[24] = {name = "Singeing Steed", ID = 184},
+	[25] = {name = "Stone Rhino", ID = 106},
+	[26] = {name = "Vortexion", ID = 99},
+}
+
 local mounts = {
 
-    -- Config
-        dollID = 37743, -- Change this to your dolls or items, item ID
-     
-        -- Main Window Messages (The first window the player sees)
-        mainTitle = "Choose a Mount!",
-        mainMsg = "Choose a mount to unlock.",
-     
-        -- Already Owned Window (The window that appears when the player already owns the addon)
-        ownedTitle = "Whoops!",
-        ownedMsg = "You already have this mount. Please choose another.",
-     
-        -- No Doll in Backpack (The window that appears when the player doesnt have the doll in their backpack)
-        dollTitle = "Whoops!",
-        dollMsg = "The mount doll must be in your backpack.",
-    -- End Config
+	-- Config
+	-- Main Window Messages (The first window the player sees)
+	mainTitle = "Escolhe uma montaria!",
+	mainMsg = "Vai liberar o bichin na hora.",
 
-    -- Mounts Table
-        [1] = {name = "Widow Queen", ID = 1},
-        [2] = {name = "Racing Bird", ID = 2},
-        [3] = {name = "War Bear", ID = 3},
-        [4] = {name = "Black Sheep", ID = 4},
-        [5] = {name = "Midnight Panther", ID = 5},
-        [6] = {name = "Draptor", ID = 6},
-        [7] = {name = "Titanica", ID = 7},
-        [8] = {name = "Tin Lizard", ID = 8},
-        [9] = {name = "Blazebringer", ID = 9},
-        [10] = {name = "Rapid Boar", ID = 10},
-        [11] = {name = "Stampor", ID = 11},
-        [12] = {name = "Undead Cavebear", ID = 12},
-        [13] = {name = "Donkey", ID = 13},
-        [14] = {name = "Tiger Slug", ID = 14},
-        [15] = {name = "Uniwheel", ID = 15},
-        [16] = {name = "Crystal Wolf", ID = 16},
-        [17] = {name = "War horse", ID = 17},
-        [18] = {name = "Kingly Deer", ID = 18},
-        [19] = {name = "Tamed Panda", ID = 19},
-        [20] = {name = "Dromedary", ID = 20},
-        [21] = {name = "King Scorpion", ID =21},
-        [22] = {name = "Rented Horse", ID = 22},
-        [23] = {name = "Armoured War Horse", ID = 23},
-        [24] = {name = "Shadow Draptor", ID =24},
-        [25] = {name = "Rented Horse", ID = 25},
-        [26] = {name = "Rented Horse", ID = 26},
-        [27] = {name = "Ladybug", ID = 27},
-        [28] = {name = "Manta Ray", ID = 28},
-        [29] = {name = "Ironblight", ID =29},
-        [30] = {name = "Magma Crawler", ID = 30},
-        [31] = {name = "Dragonling", ID = 31},
-        [32] = {name = "Gnarlhound", ID = 32},
-        [33] = {name = "Crimson Ray", ID = 33},
-        [34] = {name = "Steelbeak", ID = 34},
-        [35] = {name = "Water Buffalo", ID = 35},
-        [36] = {name = "Tombstinger", ID = 36},
-        [37] = {name = "Platesaurian", ID = 37},
-        [38] = {name = "Ursagrodon", ID = 38},
-        [39] = {name = "The Hellgrip", ID = 39},
-        [40] = {name = "Noble Lion", ID = 40},
-        [41] = {name = "Desert King", ID = 41},
-        [42] = {name = "Shock Head", ID = 42},
-        [43] = {name = "Walker", ID = 43},
-        [44] = {name = "Azudocus", ID = 44},
-        [45] = {name = "Carpacosaurus", ID = 45},
-        [46] = {name = "Death Crawler", ID = 46},
-        [47] = {name = "Flamesteed", ID = 47},
-        [48] = {name = "Jade Lion", ID = 48},
-        [49] = {name = "Jade Pincer", ID = 49},
-        [50] = {name = "Nethersteed", ID = 50},
-        [51] = {name = "Tempest", ID = 51},
-        [52] = {name = "Winter King", ID = 52},
-        [53] = {name = "Doombringer", ID = 53},
-        [54] = {name = "Woodland Prince", ID = 54},
-        [55] = {name = "Hailtorm Fury", ID = 55},
-        [56] = {name = "Siegebreaker", ID = 56},
-        [57] = {name = "Poisonbane", ID = 57},
-        [58] = {name = "Blackpelt", ID = 58},
-        [59] = {name = "Golden Dragonfly", ID = 59},
-        [60] = {name = "Steel Bee", ID = 60},
-        [61] = {name = "Copper Fly", ID = 61},
-        [62] = {name = "Tundra Rambler", ID = 62},
-        [63] = {name = "Highland Yak", ID = 63},
-        [64] = {name = "Glacier Vagabond", ID = 64},
-        [65] = {name = "Glooth Glider", ID = 65},
-        [66] = {name = "Shadow Hart", ID = 66},
-        [67] = {name = "Black Stag", ID = 67},
-        [68] = {name = "Emperor Deer", ID = 68},
-        [69] = {name = "Flying Divan", ID = 69},
-        [70] = {name = "Magic Carpet", ID = 70},
-        [71] = {name = "Floating Kashmir", ID = 71},
-        [72] = {name = "Ringtail Wazzoon", ID = 72},
-        [73] = {name = "Night Wazzoon", ID = 73},
-        [74] = {name = "Emerald Waccoon", ID = 74},
-        [75] = {name = "Flitterkatzen", ID = 75},
-        [76] = {name = "Venompaw", ID = 76},
-        [77] = {name = "Batcat", ID = 77},
-        [78] = {name = "Sea Devil", ID = 78},
-        [79] = {name = "Coralripper", ID = 79},
-        [80] = {name = "Plumfish", ID = 80},
-        [81] = {name = "Gorongra", ID = 81},
-        [82] = {name = "Noctungra", ID = 82},
-        [83] = {name = "Silverneck", ID = 83},
-        [84] = {name = "Kimera", ID = 84},
-        [85] = {name = "Kimera Devil", ID = 85},
-        [86] = {name = "Shadow Beast", ID = 86},		
-    }
-     
-local mountsDollModal = TalkAction("!mountsdoll")
+	-- Already Owned Window (The window that appears when the player already owns the addon)
+	ownedTitle = "EEEEEPA!",
+	ownedMsg = "Você ja tem essa.",
+
+	-- No Doll in Backpack (The window that appears when the player doesnt have the doll in their backpack)
+	dollTitle = "EEEEEPA!",
+	dollMsg = "Tem que ter um dos mount dolls espertinho",
+	-- End Config
+
+	-- Mounts Table
+	[1] = {name = "Cold Percht Sleigh Variant", ID = 147},
+	[2] = {name = "Bright Percht Sleigh Variant", ID = 148},
+	[3] = {name = "Dark Percht Sleigh Variant", ID = 149},
+	[4] = {name = "Dragonling", ID = 31},
+	[5] = {name = "Draptor", ID = 6},
+	[6] = {name = "Giant Beaver", ID = 201},
+	[7] = {name = "Gloothomotive", ID = 194},
+	[8] = {name = "Gnarlhound", ID = 32},
+	[9] = {name = "Gryphon", ID = 144},
+	[10] = {name = "Haze", ID = 162},
+	[11] = {name = "Lady Bug", ID = 27},
+	[12] = {name = "Manta Ray", ID = 28},
+	[13] = {name = "Midnight Panther", ID = 5},
+	[14] = {name = "Mole", ID = 119},
+	[15] = {name = "Neon Sparkid", ID = 98},
+	[16] = {name = "Scorpion King", ID = 21},
+	[17] = {name = "The Hellgrip", ID = 39},
+	[18] = {name = "Tiger Slug", ID = 14},
+	[19] = {name = "Tin Lizzard", ID = 8},
+	[20] = {name = "Titanica", ID = 7},
+	[21] = {name = "Undead Cavebear", ID = 12},
+	[22] = {name = "Uniwheel", ID = 15},
+	[23] = {name = "Ursagrodon", ID = 38},
+	[24] = {name = "White Lion", ID = 174},
+}
+
+local cheapMounts = {
+
+	-- Config
+	-- Main Window Messages (The first window the player sees)
+	mainTitle = "Escolhe uma montaria!",
+	mainMsg = "Vai liberar o bichin na hora.",
+
+	-- Already Owned Window (The window that appears when the player already owns the addon)
+	ownedTitle = "EEEEEPA!",
+	ownedMsg = "Você ja tem essa.",
+
+	-- No Doll in Backpack (The window that appears when the player doesnt have the doll in their backpack)
+	dollTitle = "EEEEEPA!",
+	dollMsg = "Tem que ter um dos mount dolls espertinho",
+	-- End Config
+
+	-- Mounts Table
+	[1] = {name = "Black Sheep", ID = 4},
+	[2] = {name = "Cold Percht Sleigh", ID = 132},
+	[3] = {name = "Bright Percht Sleigh", ID = 133},
+	[4] = {name = "Dark Percht Sleigh", ID = 134},
+	[5] = {name = "Crystal Wolf", ID = 16},
+	[6] = {name = "Donkey", ID = 13},
+	[7] = {name = "Dromedary", ID = 20},
+	[8] = {name = "Lacewing Moth", ID = 130},
+	[9] = {name = "Hibernal Moth", ID = 131},
+	[10] = {name = "Kingly Deer", ID = 18},
+	[11] = {name = "Noble Lion", ID = 40},
+	[12] = {name = "Racing Bird", ID = 2},
+	[13] = {name = "Rapid Boar", ID = 10},
+	[14] = {name = "Sparkion", ID = 94},
+	[15] = {name = "Stampor", ID = 11},
+	[16] = {name = "Tamed Panda", ID = 19},
+	[17] = {name = "Walker", ID = 43},
+	[18] = {name = "War Bear", ID = 3},
+	[19] = {name = "War Horse", ID = 17},
+	[20] = {name = "Water Buffalo", ID = 35},
+	[21] = {name = "Widow Queen", ID = 1},
+}
+
+local mountsDollModal = TalkAction("!mountdoll")
 
 function mountsDollModal.onSay(player, words, param)
-        player:sendMountWindow(mounts)
-    end
+	-- Codigo para expensive doll
+	if played:getItemCount(config.expensiveDollID) > 0 then
+		player:sendMountWindow(expensiveMounts, config.expensiveDollID)
+	end
+	-- Codigo para doll
+	if played:getItemCount(config.dollID) > 0 then
+		player:sendMountWindow(mounts, config.dollID)
+	end
+	-- Codigo para cheap doll
+	if played:getItemCount(config.cheapDollID) > 0 then
+		player:sendMountWindow(cheapMounts, config.cheapDollID)
+	end
+
+	-- Modal window design
+	local window = ModalWindow {
+		title = "EEEEEPA!", -- Title of the modal window
+		message = "Tem que ter um dos mount dolls espertinho", -- The message to be displayed on the modal window
+	}
+
+	-- Add buttons to the window (Note: if you change the names of these you must change the functions in the modal window functionallity!)
+	window:addButton("Back", buttonCallback)
+
+	-- Set what button is pressed when the player presses enter or escape
+	window:setDefaultEnterButton("Back")
+	window:setDefaultEscapeButton("Back")
+
+	-- Send the window to player
+	window:sendToPlayer(player)
+	return true
+end
 
 mountsDollModal:separator(" ")
 mountsDollModal:register()
-    
